ac7e84c11de317ee9315694244bd6e7a77fe76ce
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-07-13 00:39:31 +0530

    Updated with MEN

---
+++ models/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const consumerSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    regno:{
+        type: String,
+        required:true    
+    },
+    email:{
+        type: String,
+        required:true
+    },
+    phone:{
+        type: String,
+        required:true
+    },
+    branch:{
+        type: String,
+        required:true
+    },
+    semester:{
+        type: String,
+        required:true
+    },
+    artList:{
+        required:true
+
+    },
+    techList:{
+        required:true
+
+    }
+
+});
+
+const User = module.exports = mongoose.model('Users', consumerSchema);
+// Get User
+
+module.exports.getUser = function(callback, limit){
+    User.find(callback).limit(limit);
+    
+}
+
+module.exports.addUser = function(book, callback){
+	User.create(book, callback);
+}
